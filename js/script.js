f1a1708aae090faae89c40117f6afc3cc79ebc8f
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2023-04-14 14:27:33 -0300

    estilos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let tarjetas1 =  ["🐶", "🐰", "🐹", "🐭", "🐱","🐪" ];
 
+
 let totalTarjetas = tarjetas1.concat(tarjetas1)
+//console.log(totalTarjetas);
 
 function reparteTarjetas() {
     
@@ -14,19 +16,33 @@ function reparteTarjetas() {
         tarjeta.innerHTML = 
         '<div id="mesa">' + 
 
-            '<div class="centrarTarjeta">' +
-                 '<div class="tarjetaContenido">' + e + '</div>'
+            '<div class="centrarTarjeta">' + 
+                    '<div id="mirar" class="tarjetaContenido">' + e + '</div>'
             '</div>'
-        '</div>' 
-        ;
+            
+        '</div>' ;
     
         mesa.appendChild(tarjeta);
     }
 )
 }
 
-function descubrir() {
-    this.classlist.add("descubierta")
+
+function ver() {
+    this.classList.add("ver")
 }
 reparteTarjetas()
+
+let mostrar = document.querySelectorAll(".centrarTarjeta")
+
+mostrar.forEach(
+    function (e) {
+        e.addEventListener("click", ver)
+    }
+)
+
+
+
+
+ 
 
