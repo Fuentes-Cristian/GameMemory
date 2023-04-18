1ced48c4a14225f6ba499f3afa3c8a1138bbf4cf
Fuentes Cristian <110264073+Fuentes-Cristian@users.noreply.github.com>
2023-04-18 15:38:23 -0300

    estilos

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,19 +1,40 @@
 function iniciarContador(params) {
     let segundos = 0;
-    let minutos = 0;
-    let segundosTexto;
+    let minutos = 1;
     let minutosTexto;
+    let segundosTexto;
+    let cronometro;
 
-    function actualizaContdor(params) {
-        segundos++;
-
-        if (segundos > 59 ) {
-            segundos = 00
-            minutos++
+    function actualizaContador() {
+        segundos = segundos - 1;
+        if (segundos < 0) {
+            segundos = 59
+            minutos = minutos - 1;
         }
+        if (minutos < 0) {
+            segundos = 0
+            minutos = 0;
+            clearInterval(cronometro)
+        }
+        segundosTexto = segundos;
+        minutosTexto  = minutos;
+
+        if (segundos < 10) {
+            segundosTexto = '0' + segundos
+        }
+        if (minutos < 10) {
+            minutosTexto = '0' + minutos
+        }
+        
+        document.querySelector("#minutos").innerText =  minutosTexto
+        document.querySelector("#segundos").innerText =  segundosTexto
+
+
     }
 
-    setInterval(actualizaContdor, 1000)
-}
 
+
+
+ cronometro = setInterval(actualizaContador, 1000)
+}
 
